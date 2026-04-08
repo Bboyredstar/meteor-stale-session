0569c3d6b01522ede0e5e3beadafc812dc50e937
Tima Leonov <mbdoy213@gmail.com>
2026-04-08 13:19:29 +0400

    #release Bump stale-session package version to 0.1.2

--- example/packages/stale-session/package.js
+++ example/packages/stale-session/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'bboyredstar:stale-session',
-  version: '0.1.1',
+  version: '0.1.2',
   summary: 'Activity detection for the session timeout',
   git: 'https://github.com/Bboyredstar/meteor-stale-session',
   documentation: './README.md',
